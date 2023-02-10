90fc7edd66677abf43cfd29855eeb52d18aea24b
Santosh <n170425@rguktn.ac.in>
2023-02-10 22:02:48 +0530

    deleted

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-while traci.simulation.getMinExpectedNumber() > 0:
-    # Get the data for the current vehicle
-    vehicle_data = traci.vehicle.getVehicleData(vehicle_id)
-
-    # Make a decision based on the vehicle data
-    make_decision(vehicle_data)
-
-    # Advance the simulation by one step
-    traci.simulationStep()

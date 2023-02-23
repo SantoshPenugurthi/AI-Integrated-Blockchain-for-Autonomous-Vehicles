0fcaab4e81962f11b01265ecfaca75c4797de633
Santosh <n170425@rguktn.ac.in>
2023-02-23 13:30:54 +0530

    blockchain

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,42 +1,27 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract VehicleControl {
-
-    // Declare an instance of the Traci library for Python
-    Traci traci = Traci("localhost", 8813);
-
-    // Define a function to start the simulation
-    function startSimulation() public {
-        traci.start(["sumo-gui","-c", "Sumo.sumocfg"]);
+contract SumoVehicleInfo {
+    // Define a struct to represent a vehicle
+    struct Vehicle {
+        string id;
+        uint256 speed;
+        uint256 lane;
+        uint256 position;
     }
 
-    // Define a function to stop the simulation
-    function stopSimulation() public {
-        traci.close();
+    // Define a mapping to store vehicle data by ID
+    mapping (string => Vehicle) vehicles;
+
+    // Function to retrieve vehicle data by ID
+    function getVehicleInfo(string memory id) public view returns (uint256, uint256, uint256) {
+        Vehicle memory vehicle = vehicles[id];
+        return (vehicle.speed, vehicle.lane, vehicle.position);
     }
 
-    // Define a function to set the speed of a vehicle
-    function setSpeed(string memory vehicleID, uint speed) public {
-        traci.vehicle.setSpeed(vehicleID, speed);
-    }
-
-    // Define a function to get the IDs of all the vehicles in the simulation
-    function getVehicleIDs() public view returns (string[] memory) {
-        return traci.vehicle.getIDList();
-    }
-
-    // Define a function to get the road ID of a vehicle
-    function getRoadID(string memory vehicleID) public view returns (string memory) {
-        return traci.vehicle.getRoadID(vehicleID);
-    }
-
-    // Define a function to get the current edge of a vehicle
-    function getCurrentEdge(string memory vehicleID) public view returns (string memory) {
-        return traci.vehicle.getRoadID(vehicleID);
-    }
-
-    // Define a function to set the route of a vehicle
-    function setRoute(string memory vehicleID, string[] memory route) public {
-        traci.vehicle.setRoute(vehicleID, route);
+    // Function to update vehicle data by ID
+    function updateVehicleInfo(string memory id, uint256 speed, uint256 lane, uint256 position) public {
+        Vehicle memory vehicle = Vehicle(id, speed, lane, position);
+        vehicles[id] = vehicle;
     }
 }
